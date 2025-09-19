8c8f6f3355899c62e435f3a9ee9af596e231c60e
J.VenuGopal Azad <95510274+Azad117@users.noreply.github.com>
2025-09-19 14:22:28 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -32,3 +32,31 @@ print("Decision Tree:", tree)
 
 sample = {"Outlook": "Sunny","Temperature":"Cool","Humidity":"High","Wind":"Strong"}
 print("Classification:", predict(tree, sample))
+
+
+
+
+
+
+//Input :Outlook,Temperature,Humidity,Wind,PlayTennis
+Sunny,Hot,High,Weak,No
+Sunny,Hot,High,Strong,No
+Overcast,Hot,High,Weak,Yes
+Rain,Mild,High,Weak,Yes
+Rain,Cool,Normal,Weak,Yes
+Rain,Cool,Normal,Strong,No
+Overcast,Cool,Normal,Strong,Yes
+Sunny,Mild,High,Weak,No
+Sunny,Cool,Normal,Weak,Yes
+Rain,Mild,Normal,Weak,Yes
+Sunny,Mild,Normal,Strong,Yes
+Overcast,Mild,High,Strong,Yes
+Overcast,Hot,Normal,Weak,Yes
+Rain,Mild,High,Strong,No
+
+
+
+
+
+
+
